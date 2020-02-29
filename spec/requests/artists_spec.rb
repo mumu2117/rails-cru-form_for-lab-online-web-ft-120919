6e855404b77fd8ed635f526612f896db6e6cc8c8
Jennifer Miller <jmillerarcher@me.com>
2020-02-29 15:29:11 +0000

    Automatically backed up by Learn

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Artists", type: :request do
-  describe "GET /artists" do
-    it "works! (now write some real specs)" do
-      get artists_path
-      expect(response).to have_http_status(200)
-    end
-  end
-end
+# RSpec.describe "Artists", type: :request do
+#   describe "GET /artists" do
+#     it "works! (now write some real specs)" do
+#       get artists_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
