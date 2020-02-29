24c1e1fe43b6693283bad0bb523cb9a31ca3646c
Jennifer Miller <jmillerarcher@me.com>
2020-02-29 15:23:08 +0000

    Automatically backed up by Learn

---
+++ spec/views/songs/edit.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "songs/edit", type: :view do
+  before(:each) do
+    @song = assign(:song, Song.create!(
+      :name => "MyString"
+    ))
+  end
+
+  it "renders the edit song form" do
+    render
+
+    assert_select "form[action=?][method=?]", song_path(@song), "post" do
+
+      assert_select "input#song_name[name=?]", "song[name]"
+    end
+  end
+end
